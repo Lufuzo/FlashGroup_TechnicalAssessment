f70d463aaae3c5f4955aa3dbf601bc02eb21e7d1
Lufuzo <lufuzolm@gmail.com>
2023-05-23 06:36:40 +0200

    Latest changes on the CLient app

--- SQL_SanitizeWordsMVC_Client/Controllers/WordController.cs
+++ SQL_SanitizeWordsMVC_Client/Controllers/WordController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Text;
 using NuGet.ProjectModel;
 using SQL_SanitizeWordsMVC_Client.Models;
+using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SQL_SanitizeWordsMVC_Client.Controllers
 {
     public class WordController : Controller
     {
         private readonly APIGateway apiGateway;
+
+        
 
 
         public WordController(APIGateway _apiGateway)
@@ -31,9 +40,25 @@ namespace SQL_SanitizeWordsMVC_Client.Controllers
         [HttpPost]
         public IActionResult Create(Word word)
         {
-            apiGateway.CreateWord(word);
+
+            var checkWord =  GetWords();
+
+            var result = checkWord.Where(x => x.FileValues.Any(y => y.Equals(word.value))).ToList();
+            if (result.Count == 0)
+            {
+                apiGateway.CreateWord(word);
+            }
+            else
+            {
+                string mess = "Dangerous input!!!!";
+                ViewData["message"] = mess;
+            }
+
+            
             return RedirectToAction("Index");
         }
+       
+
 
         [HttpGet]
         public IActionResult Details(int id)
@@ -76,5 +101,49 @@ namespace SQL_SanitizeWordsMVC_Client.Controllers
             apiGateway.DeleteWord(word.id);
             return RedirectToAction("Index");
         }
-    }
+
+        // create list 
+
+        public class FileValue
+        {
+            public List<string> FileValues { get; set; }
+
+            public FileValue()
+            {
+                FileValues = new List<string>();
+            }
+        }
+        // reading a file
+        private static List<FileValue> GetWords()
+        {
+            string path = "C:\\Users\\Lungelo Mbalane\\Documents\\Visual Studio 2022\\SQL_Words_Application\\SQL_SanitizeWordsMVC_Client\\SQL_SanitizeWordsMVC_Client\\Data\\sql_sensitive_list.txt";
+
+            var fileValues = new List<FileValue>();
+            var fileValue = new FileValue();
+           
+            List<String[]> arrayList = new List<String[]>();
+            //string [] words = File.ReadAllLines(path).ToString();
+            using (StreamReader file = new StreamReader(path))
+            {
+                //int counter = 0;
+                string line = String.Empty;
+
+                while (!String.IsNullOrEmpty(line = file.ReadLine()))
+                {
+                    fileValue.FileValues.Add(line);
+                    fileValues.Add(fileValue);
+
+                   
+                }
+
+    
+                file.Close();
+
+
+            }
+
+
+            return fileValues;
+      }
+}
 }
